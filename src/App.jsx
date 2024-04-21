c27cfb403763c4f76ce1b7a94bfb4bae04246566
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-04-21 11:37:02 -0700

    hotfix/Add Mockups folder and move all elements into Mockup_v1.jsx and display into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import FileUploader from "./components/FileUploader/FileUploader";
-import PDFViewer from "./components/PDFViewer/PDFViewer";
-import ExtractedText from "./components/ExtractedText/ExtractedText";
+import Mockup_v1 from "./Mockups/Mockup_v1/Mockup_v1";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="flex">
-        <div className="mx-4 my-4 h-screen w-1/2 border-r">
-          <div className="mt-4">
-            <div className="flex">
-              <FileUploader />
-            </div>
-          </div>
-          <PDFViewer />
-        </div>
-        <hr className="my-auto" />
-        <div className="mx-4 my-4 h-screen w-1/2">
-          <div>
-            <ExtractedText />
-          </div>
-        </div>
-      </div>
+      <Mockup_v1 />
     </>
   );
 }
